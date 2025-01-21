efa692d24f1ebe8e402e19c65b6586dab88f176d
Chris Norman <chris.norman1989@outlook.com>
2025-01-21 21:44:20 +0000

    Saved a file.

---
+++ lib/src/widgets/performable_actions/performable_action_menu_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../selected_icon.dart';
+import 'performable_action.dart';
+
+/// A [MenuItemButton] which represents an [action].
+class PerformableActionMenuItem extends StatelessWidget {
+  /// Create an instance.
+  const PerformableActionMenuItem({
+    required this.action,
+    this.autofocus = false,
+    super.key,
+  });
+
+  /// The action to use.
+  final PerformableAction action;
+
+  /// Whether the [MenuItemButton] should be autofocused.
+  final bool autofocus;
+
+  /// Build the widget.
+  @override
+  Widget build(final BuildContext context) {
+    final invoke = action.invoke;
+    final activator = action.activator;
+    final selected = action.checked;
+    return Semantics(
+      checked: selected,
+      selected: action.checked,
+      child: MenuItemButton(
+        autofocus: autofocus,
+        onPressed: invoke,
+        shortcut: activator,
+        trailingIcon:
+            selected == null ? null : SelectedIcon(selected: selected),
+        child: Text(action.name),
+      ),
+    );
+  }
+}
